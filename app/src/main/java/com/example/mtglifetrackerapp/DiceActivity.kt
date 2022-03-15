9c58a2ed0d596e4890464ace2a0f8aaefc13e195
birke <birkettlewis@gmail.com>
2022-03-15 15:31:19 +0000

    Submit function begun

--- app/src/main/java/com/example/mtglifetrackerapp/DiceActivity.kt
+++ app/src/main/java/com/example/mtglifetrackerapp/DiceActivity.kt
@@ -1,13 +1,18 @@
 package com.example.mtglifetrackerapp
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mtglifetrackerapp.databinding.ActivityDiceBinding
 
 class DiceActivity : AppCompatActivity() {
     private lateinit var binding : ActivityDiceBinding
+    private lateinit var sA : SpinnerActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityDiceBinding.inflate(layoutInflater)
@@ -28,12 +33,50 @@ class DiceActivity : AppCompatActivity() {
         }
 
 
-
-
+        val but : Button = binding.Submit
+        but.setOnClickListener { submit() }
         //roll different types of dice + quantity
-
-
     }
 
+    private fun submit() {
+        //if wrong input (0 / too many dice) on edittext then reset values and don't perform submit
+        val amountStr = binding.quantity.text.toString()
+        val amount : Int = Integer.parseInt(amountStr)
+        val results = binding.results
+        val spin = binding.diceSpinner
+
+        if (amount > 4) {   //Too many
+
+        } else {        //Do roll/spins
+            //get answer from spinner
+            //animated roll/flip??
+            //call different functions in for loop depending on quantity
+            //display results in the results textview
+        }
+    }
+
+    private fun rollDx(x : Int) : Int {
+        return (1..x).shuffled().first()
+    }
+
+    private fun coinFlip() : String {
+        return listOf("Heads", "Tails").random()
+    }
+
+}
+
+
+class SpinnerActivity : Activity(), AdapterView.OnItemSelectedListener {
+
+
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+    }
 
 }
