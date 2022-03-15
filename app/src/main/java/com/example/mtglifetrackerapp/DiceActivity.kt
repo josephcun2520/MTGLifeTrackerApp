9adf476d7990a228e049494de666382c75f71606
birke <birkettlewis@gmail.com>
2022-03-15 14:05:48 +0000

    Added drop down menu and simple UI for dice activity

--- app/src/main/java/com/example/mtglifetrackerapp/DiceActivity.kt
+++ app/src/main/java/com/example/mtglifetrackerapp/DiceActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mtglifetrackerapp
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mtglifetrackerapp.databinding.ActivityDiceBinding
 
@@ -11,6 +13,24 @@ class DiceActivity : AppCompatActivity() {
         binding = ActivityDiceBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val spinner : Spinner = binding.diceSpinner
+        //Create array adapter from string array
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.dice_array,
+            android.R.layout.simple_spinner_item).also {
+                adapter ->
+            //Specify layout to use for list
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            //Apply adapter to spinner
+            spinner.adapter = adapter
+        }
+
+
+
+
+        //roll different types of dice + quantity
 
 
     }
