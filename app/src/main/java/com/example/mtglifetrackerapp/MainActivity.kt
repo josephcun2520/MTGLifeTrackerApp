3d99eddf477163d09d7f24b8506def7c9e6122d0
birke <birkettlewis@gmail.com>
2022-03-22 12:21:16 +0000

    added more to lose health function. notification code begun

--- app/src/main/java/com/example/mtglifetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/mtglifetrackerapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Vibrator
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.example.mtglifetrackerapp.databinding.ActivityMainBinding
 import java.util.*
 
@@ -13,7 +14,7 @@ private lateinit var binding : ActivityMainBinding
 private var players = Vector<PlayerData>()
 var pattern = longArrayOf(0, 250, 500, 250, 500)
 var winPattern = longArrayOf(0, 500, 250, 500, 250)
-
+private lateinit var winner : PlayerData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,6 +50,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        fun declareWinner() {
+            //var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                //.setSmallIcon(R.drawable.notification_icon)
+                //.setContentTitle("My notification")
+                //.setContentText("Much longer text that cannot fit one line...")
+                //.setStyle(NotificationCompat.BigTextStyle()
+                    //.bigText("Much longer text that cannot fit one line..."))
+                //.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            //TODO: add button to share data to other apps / enable pre-existing one
+        }
+
         fun onLoseHealth(playerIdx : Int, vib : Vibrator) {
 
             var gameOver : Boolean = false
@@ -56,10 +69,13 @@ class MainActivity : AppCompatActivity() {
             for (i in players.indices) {
                 if (players[i].health > 0) {
                     aliveCount++
+                    winner = players[i] //If only one alive then winner is correct
                 }
             }
-            if (aliveCount > 1)
+            if (aliveCount == 1) {
                 gameOver = true
+                declareWinner()
+            }
 
             if (gameOver) {
                 vib.vibrate(winPattern, -1) //Win pattern
