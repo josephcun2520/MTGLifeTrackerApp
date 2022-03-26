881b326f05e9b79dc5fd6681dc548e61c02fe5b7
Joseph Cunningham <joseph.cunningham2520@gmail.com>
2022-03-26 21:36:03 +0000

    Combined dice section from other branch, currently crashing at startup

--- app/src/main/java/com/example/mtglifetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/mtglifetrackerapp/MainActivity.kt
@@ -2,11 +2,44 @@ package com.example.mtglifetrackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.ImageButton
+import com.example.mtglifetrackerapp.DiceActivity
+import com.example.mtglifetrackerapp.databinding.ActivityMainBinding
+import java.util.*
+
+private lateinit var binding : ActivityMainBinding
+private var players = Vector<PlayerData>()
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         //TODO: use putextra to save state between activity transitions
+
+        players.addAll(listOf(PlayerData(1, 20, false), PlayerData(2, 20, false), PlayerData(3,20, false), PlayerData(4,20, false)))
+        //TODO: make id unique
+
+        val but : ImageButton = binding.diceButton
+        but.setOnClickListener {
+            val intent = Intent (this@MainActivity, DiceActivity::class.java)
+            startActivity(intent)
+        }
+
+        @Suppress("RedundantIf")
+        fun selectMonarch(idx : Int) {                  //0 indexed
+            for (i in players.indices) {
+                if (i == idx) {
+                    players[i].isMonarch = true
+                }
+                else {
+                    players[i].isMonarch = false
+                }
+            }
+        }
+
+        //TODO: use put extra to save state between activity transitions
     }
 }
---
+++ app/src/main/java/com/example/mtglifetrackerapp/PlayerData.kt
@@ -0,0 +1,4 @@
+package com.example.mtglifetrackerapp
+
+data class PlayerData(val id : Int, var health : Int, var isMonarch : Boolean)
+//TODO: add data representation for counters, commander damage, monarch?
