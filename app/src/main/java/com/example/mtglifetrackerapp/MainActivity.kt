ba52bd9422d9ccb991015de20a4b05032382f08c
birke <birkettlewis@gmail.com>
2022-03-29 15:15:18 +0100

    Added untested share code, removed some warnings and TODOs

--- app/src/main/java/com/example/mtglifetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/mtglifetrackerapp/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.mtglifetrackerapp
 
 import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.PendingIntent.*
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.app.PendingIntent.getActivity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -10,7 +10,6 @@ import android.os.Vibrator
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat.getSystemService
 import com.example.mtglifetrackerapp.databinding.ActivityMainBinding
 import java.util.*
 
@@ -24,27 +23,6 @@ lateinit var notificationManager: NotificationManager
 var gameOver = false
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val v = getSystemService(VIBRATOR_SERVICE) as Vibrator
-
-        players.addAll(listOf(PlayerData(1, 20, false, 0, 0, 0, 0),
-            PlayerData(2, 20, false, 0, 0, 0, 0),
-            PlayerData(3,20, false, 0, 0, 0, 0),
-            PlayerData(4,20, false, 0, 0, 0, 0)))
-        //TODO: make id unique / String or use database?
-
-        //TODO: connect the buttons up to the data class
-
-        val but : Button = binding.diceButton
-        but.setOnClickListener {
-            val intent = Intent (this@MainActivity, DiceActivity::class.java)
-            startActivity(intent)
-        }
 
         @Suppress("RedundantIf")
         fun selectMonarch(idx : Int) {                  //0 indexed
@@ -58,11 +36,11 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        fun winnerNotify() {
-            val channel_ID = "MTG"
+        private fun winnerNotify() {
+            val channelID = "MTG"
             val pendingIntent = getActivity(this, 0, intent, FLAG_UPDATE_CURRENT)
 
-            var builder = NotificationCompat.Builder(this, channel_ID)
+            val builder = NotificationCompat.Builder(this, channelID)
                 //.setSmallIcon(R.drawable.notification_icon)
                 .setContentTitle("Game Over")
                 .setContentText("Winner is Player " + winner.id)
@@ -73,17 +51,16 @@ class MainActivity : AppCompatActivity() {
 
                 //Send notification
             notificationManager.notify(1, builder.build())
-            //TODO: add button to share data to other apps / enable pre-existing one
         }
 
-        fun shareResults() {
+        private fun shareResults() {
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
                 putExtra(Intent.EXTRA_TEXT, "Winner: Player " + winner.id + "\nHealth: " + winner.health)
                 type = "text/plain"
             }
 
-            val shareIntent = Intent.createChooser(sendIntent, null)
+            val shareIntent = Intent.createChooser(sendIntent, "Share Via")
             startActivity(shareIntent)
         }
 
@@ -101,10 +78,9 @@ class MainActivity : AppCompatActivity() {
             if (aliveCount <= 1) {
                 winnerNotify()
                 vib.vibrate(winPattern, -1) //Win pattern
-                //TODO: disable all buttons & enable the share button / screen
+                binding.shareButton.isEnabled = true    //Enable the share button to be pressed
             } else {
                 if (players[playerIdx].health - 1 == 0) {    //Dead
-                    //TODO: disable the button here
                     --players[playerIdx].health
                     vib.vibrate(pattern, -1)       //Player dead
                 } else {
@@ -114,6 +90,37 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        //TODO: use put extra to save state between activity transitions
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val v = getSystemService(VIBRATOR_SERVICE) as Vibrator
+
+        players.addAll(listOf(PlayerData(1, 20, false, 0, 0, 0, 0),
+            PlayerData(2, 20, false, 0, 0, 0, 0),
+            PlayerData(3,20, false, 0, 0, 0, 0),
+            PlayerData(4,20, false, 0, 0, 0, 0)))
+        //TODO: make id unique / String or use database?
+        //TODO: connect the buttons up to the data class
+
+        //Default winner
+        winner = PlayerData(-1, -1, false, 0, 0, 0, 0)
+
+        val but : Button = binding.diceButton
+        but.setOnClickListener {
+            val intent = Intent (this@MainActivity, DiceActivity::class.java)
+            startActivity(intent)
+        }
+
+        val shareBut : Button = binding.shareButton
+        shareBut.setOnClickListener {
+            shareResults()
+        }
+        //shareBut.isEnabled = false
+        //TODO: RE-ENABLE THIS LINE
+
     }
 }
--- app/src/main/java/com/example/mtglifetrackerapp/PlayerData.kt
+++ app/src/main/java/com/example/mtglifetrackerapp/PlayerData.kt
@@ -4,4 +4,3 @@ data class PlayerData(val id : Int, var health : Int,
                       var isMonarch : Boolean, var poisonCounters : Int,
                       var energyCounters : Int, var expCounters : Int,
                       var bloodCounters : Int)
-//TODO: add data representation for counters, commander damage,
