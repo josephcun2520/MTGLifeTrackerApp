223e44418e759c5d47c149aff2e98059e659ef71
birke <birkettlewis@gmail.com>
2022-03-29 16:22:20 +0100

    Changed dialog fragment to cover whole screen and use xml layout file

--- app/src/main/java/com/example/mtglifetrackerapp/DiceFragment.kt
+++ app/src/main/java/com/example/mtglifetrackerapp/DiceFragment.kt
@@ -1,37 +1,38 @@
 package com.example.mtglifetrackerapp
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import kotlinx.coroutines.NonCancellable.cancel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 class DiceFragment : DialogFragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
-            // Use the Builder class for convenient dialog construction
-            val builder = AlertDialog.Builder(it)
-            builder.setMessage(R.string.dialog_start_game)
-                .setPositiveButton(R.string.start,
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // START THE GAME!
-                    })
-                .setNegativeButton(R.string.cancel,
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // User cancelled the dialog
-                    })
-            // Create the AlertDialog object and return it
-            builder.create()
-        } ?: throw IllegalStateException("Activity cannot be null")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val metrics : DisplayMetrics = resources.displayMetrics
+        val width = metrics.widthPixels
+        val height = metrics.heightPixels
+        dialog?.window?.setLayout(width, height)
+
+        return inflater.inflate(R.layout.fragment_dice, container)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (dialog != null) {
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.MATCH_PARENT
+            dialog!!.window!!.setLayout(width, height)
+        }
     }
 
 }
