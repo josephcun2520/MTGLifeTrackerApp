7d2339d9fc01a709f20ba57fcd1af756c41e9548
josephcun2520 <joseph.cunningham2520@gmail.com>
2022-03-31 05:37:21 +0100

    Cleaned up code in health fragment class significantly

--- app/src/main/java/com/example/mtglifetrackerapp/HealthFragment.kt
+++ app/src/main/java/com/example/mtglifetrackerapp/HealthFragment.kt
@@ -1,12 +1,10 @@
 package com.example .mtglifetrackerapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
-import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
-import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.ImageButton
@@ -14,69 +12,56 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-
+import kotlin.math.abs
 
 class HealthFragment : Fragment() {
-    var x1 = 0
-    var x2 = 0
-    var y1 = 0
-    var y2 = 0
-    var dx = 0
-    var dy = 0
-    var healthCount : TextView? = null
-    var commanderDmg1 : TextView? = null
-    var commanderDmg2 : TextView? = null
-    var commanderDmg3: TextView? = null
-    var cDmgUp1Btn : ImageButton? = null
-    var cDmgUp2Btn : ImageButton? = null
-    var cDmgUp3Btn : ImageButton? = null
-    var cDmgDown1Btn : ImageButton? = null
-    var cDmgDown2Btn : ImageButton? = null
-    var cDmgDown3Btn : ImageButton? = null
-    var upButton : ImageButton? = null
-    var downButton : ImageButton? = null
-    var bloodImg : ImageView? = null
-    var energyImg : ImageView? = null
-    var poisonImg : ImageView? = null
-    var token1 : TextView? = null
-    var token2 : TextView? = null
-    var token3 : TextView? = null
-    var token1UpBtn : ImageButton? = null
-    var token2UpBtn : ImageButton? = null
-    var token3UpBtn : ImageButton? = null
-    var token1DownBtn : ImageButton? = null
-    var token2DownBtn : ImageButton? = null
-    var token3DownBtn : ImageButton? = null
-    var bloodTokens = 0
-    var energyTokens = 0
-    var poisonTokens = 0
-    var leftButton : ImageButton? = null
-    var rightButton : ImageButton? = null
-    var health : Int = 40
-    var cDmg1 : Int = 0
-    var cDmg2 : Int = 0
-    var cDmg3 : Int = 0
-    var page : Int = 1
-    var playNo = 0
+    private var healthCount : TextView? = null
+    private var commanderDmg1 : TextView? = null
+    private var commanderDmg2 : TextView? = null
+    private var commanderDmg3: TextView? = null
+    private var cDmgUp1Btn : ImageButton? = null
+    private var cDmgUp2Btn : ImageButton? = null
+    private var cDmgUp3Btn : ImageButton? = null
+    private var cDmgDown1Btn : ImageButton? = null
+    private var cDmgDown2Btn : ImageButton? = null
+    private var cDmgDown3Btn : ImageButton? = null
+    private var upButton : ImageButton? = null
+    private var downButton : ImageButton? = null
+    private var bloodImg : ImageView? = null
+    private var energyImg : ImageView? = null
+    private var poisonImg : ImageView? = null
+    private var token1 : TextView? = null
+    private var token2 : TextView? = null
+    private var token3 : TextView? = null
+    private var token1UpBtn : ImageButton? = null
+    private var token2UpBtn : ImageButton? = null
+    private var token3UpBtn : ImageButton? = null
+    private var token1DownBtn : ImageButton? = null
+    private var token2DownBtn : ImageButton? = null
+    private var token3DownBtn : ImageButton? = null
+    private var bloodTokens = 0
+    private var energyTokens = 0
+    private var poisonTokens = 0
+    private var leftButton : ImageButton? = null
+    private var rightButton : ImageButton? = null
+    private var health : Int = 40
+    private var cDmg1 : Int = 0
+    private var cDmg2 : Int = 0
+    private var cDmg3 : Int = 0
+    private var page : Int = 1
+    private var playNo = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    fun changeHealth(amount:Int): View.OnClickListener? {
+    private fun changeHealth(amount:Int): View.OnClickListener? {
         health += amount
-        healthCount?.setText(health.toString())
+        healthCount?.text = health.toString()
 
         if (amount >= 5 || amount <= -5) {
             healthCount?.animation = AnimationUtils.loadAnimation(context,R.anim.shake_animation)
-            var text = ""
-            if (amount < 0) {
+            val text = if (amount < 0) {
                 val posAmount = amount * -1
-                text = "Player $playNo lost $posAmount health!"
-            }
-            else
-            {
-                text = "Player $playNo gained $amount health!"
+                "Player $playNo lost $posAmount health!"
+            } else {
+                "Player $playNo gained $amount health!"
             }
             val duration = Toast.LENGTH_SHORT
 
@@ -87,31 +72,33 @@ class HealthFragment : Fragment() {
         return null
     }
 
-    fun changeCommanderDamage(amount:Int,player:Int):View.OnClickListener? {
-        if (player == 1){
-            cDmg1 += amount
-            commanderDmg1?.setText(cDmg1.toString())
-        }
-        else if (player == 2) {
-            cDmg2 += amount
-            commanderDmg2?.setText(cDmg2.toString())
-        }
-        else if (player == 3) {
-            cDmg3 += amount
-            commanderDmg3?.text = cDmg3.toString()
+    private fun changeCommanderDamage(amount:Int, player:Int):View.OnClickListener? {
+        when (player) {
+            1 -> {
+                cDmg1 += amount
+                commanderDmg1?.text = cDmg1.toString()
+            }
+            2 -> {
+                cDmg2 += amount
+                commanderDmg2?.text = cDmg2.toString()
+            }
+            3 -> {
+                cDmg3 += amount
+                commanderDmg3?.text = cDmg3.toString()
+            }
         }
 
         return null
     }
 
-    fun changeTokens(amount:Int,type:Int):View.OnClickListener? {
+    private fun changeTokens(amount:Int, type:Int):View.OnClickListener? {
         if (type == 1){
             bloodTokens += amount
-            token1?.setText(bloodTokens.toString())
+            token1?.text = bloodTokens.toString()
         }
         else if (type == 2) {
             energyTokens += amount
-            token2?.setText(energyTokens.toString())
+            token2?.text = energyTokens.toString()
         }
         else if (type == 3) {
             poisonTokens += amount
@@ -121,7 +108,7 @@ class HealthFragment : Fragment() {
             {
                 health = 0
                 healthCount?.text = health.toString()
-                var text = "Player $playNo dies to poison!"
+                val text = "Player $playNo dies to poison!"
                 val duration = Toast.LENGTH_SHORT
                 val toast = Toast.makeText(context, text, duration)
                 toast.show()
@@ -237,6 +224,7 @@ class HealthFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_health, container, false)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         upButton = view.findViewById(R.id.upButton)
@@ -266,228 +254,70 @@ class HealthFragment : Fragment() {
         energyImg = view.findViewById(R.id.energyImage)
         poisonImg = view.findViewById(R.id.poisonImage)
 
+        healthCount?.setOnTouchListener { _, event ->
+            when (getDirectionOfSwipe(event)) {
+                "up" -> changeHealth(-5)
+                "down" -> changeHealth(5)
+                "bigUp" -> changeHealth(-10)
+                "bigDown" -> changeHealth(10)
+            }
+            true
+        }
+        commanderDmg1?.setOnTouchListener { _, event ->
+            when (getDirectionOfSwipe(event)) {
+                "up" -> changeCommanderDamage(-5, 1)
+                "down" -> changeCommanderDamage(5, 1)
+                "bigUp" -> changeCommanderDamage(-10, 1)
+                "bigDown" -> changeCommanderDamage(10, 1)
+            }
+            true
+        }
+        commanderDmg2?.setOnTouchListener { _, event ->
+            when (getDirectionOfSwipe(event)) {
+                "up" -> changeCommanderDamage(-5, 2)
+                "down" -> changeCommanderDamage(5, 2)
+                "bigUp" -> changeCommanderDamage(-10, 2)
+                "bigDown" -> changeCommanderDamage(10, 2)
+            }
+            true
+        }
+        commanderDmg3?.setOnTouchListener { _, event ->
+            when (getDirectionOfSwipe(event)) {
+                "up" -> changeCommanderDamage(-5, 3)
+                "down" -> changeCommanderDamage(5, 3)
+                "bigUp" -> changeCommanderDamage(-10, 3)
+                "bigDown" -> changeCommanderDamage(10, 3)
+            }
+            true
+        }
+        token1?.setOnTouchListener { _, event ->
+            when (getDirectionOfSwipe(event)) {
+                "up" -> changeTokens(-5, 2)
+                "down" -> changeTokens(5, 2)
+                "bigUp" -> changeTokens(-10, 2)
+                "bigDown" -> changeTokens(10, 2)
+            }
+            true
+        }
+        token2?.setOnTouchListener { _, event ->
+            when (getDirectionOfSwipe(event)) {
+                "up" -> changeTokens(-5, 3)
+                "down" -> changeTokens(5, 3)
+                "bigUp" -> changeTokens(-10, 3)
+                "bigDown" -> changeTokens(10, 3)
+            }
+            true
+        }
+        token3?.setOnTouchListener { _, event ->
+            when (getDirectionOfSwipe(event)) {
+                "up" -> changeTokens(-5, 3)
+                "down" -> changeTokens(5, 3)
+                "bigUp" -> changeTokens(-10, 3)
+                "bigDown" -> changeTokens(10, 3)
+            }
+            true
+        }
         upButton?.setOnClickListener{changeHealth(1)}
-
-        //TODO: Move on touch listener to separate function so that it can be used for many elements without needing 30 lines for each
-        healthCount?.setOnTouchListener(OnTouchListener { v, event ->
-            val direction : String = getDirectionOfSwipe(event)
-            if (direction == "up")
-            {
-                changeHealth(-5)
-            }
-            else if (direction == "down")
-            {
-                changeHealth(5)
-            }
-            else if (direction == "bigUp")
-            {
-                changeHealth(-10)
-            }
-            else if (direction == "bigDown")
-            {
-                changeHealth(10)
-            }
-            true
-        })
-
-        commanderDmg1?.setOnTouchListener(OnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                x1 = event.getX().toInt()
-                y1 = event.getY().toInt()
-            } else if (event.action == MotionEvent.ACTION_UP) {
-                x2 = event.getX().toInt()
-                y2 = event.getY().toInt()
-                dx = x2-x1
-                dy = y2-y1
-
-                //TODO: Make this function take distance/angle into account
-
-                if(Math.abs(dx) > Math.abs(dy))
-                {
-                    if (dx>0){
-                        changePage(-1)
-                    }
-                    else if (dx<0)
-                    {
-                        changePage(1)
-                    }
-                }
-                else
-                {
-                    if (dy>0)   //up
-                        changeCommanderDamage(-1,1)
-                    else if (dy<0)  //down
-                        changeCommanderDamage(1,1)
-                }
-            }
-            true
-        })
-
-        commanderDmg2?.setOnTouchListener(OnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                x1 = event.getX().toInt()
-                y1 = event.getY().toInt()
-            } else if (event.action == MotionEvent.ACTION_UP) {
-                x2 = event.getX().toInt()
-                y2 = event.getY().toInt()
-                dx = x2-x1
-                dy = y2-y1
-
-                //TODO: Make this function take distance/angle into account
-
-                if(Math.abs(dx) > Math.abs(dy))
-                {
-                    if (dx>0){
-                        changePage(-1)
-                    }
-                    else if (dx<0)
-                    {
-                        changePage(1)
-                    }
-                }
-                else
-                {
-                    if (dy>0)   //up
-                        changeCommanderDamage(-1,2)
-                    else if (dy<0)  //down
-                        changeCommanderDamage(1,2)
-                }
-            }
-            true
-        })
-
-        commanderDmg3?.setOnTouchListener(OnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                x1 = event.getX().toInt()
-                y1 = event.getY().toInt()
-            } else if (event.action == MotionEvent.ACTION_UP) {
-                x2 = event.getX().toInt()
-                y2 = event.getY().toInt()
-                dx = x2-x1
-                dy = y2-y1
-
-                //TODO: Make this function take distance/angle into account
-
-                if(Math.abs(dx) > Math.abs(dy))
-                {
-                    if (dx>0){
-                        changePage(-1)
-                    }
-                    else if (dx<0)
-                    {
-                        changePage(1)
-                    }
-                }
-                else
-                {
-                    if (dy>0)   //up
-                        changeCommanderDamage(-1,3)
-                    else if (dy<0)  //down
-                        changeCommanderDamage(1,3)
-                }
-            }
-            true
-        })
-
-        token1?.setOnTouchListener(OnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                x1 = event.getX().toInt()
-                y1 = event.getY().toInt()
-            } else if (event.action == MotionEvent.ACTION_UP) {
-                x2 = event.getX().toInt()
-                y2 = event.getY().toInt()
-                dx = x2-x1
-                dy = y2-y1
-
-                //TODO: Make this function take distance/angle into account
-
-                if(Math.abs(dx) > Math.abs(dy))
-                {
-                    if (dx>0){
-                        changePage(-1)
-                    }
-                    else if (dx<0)
-                    {
-                        changePage(1)
-                    }
-                }
-                else
-                {
-                    if (dy>0)   //up
-                        changeTokens(-1,1)
-                    else if (dy<0)  //down
-                        changeTokens(1,1)
-                }
-            }
-            true
-        })
-
-        token2?.setOnTouchListener(OnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                x1 = event.getX().toInt()
-                y1 = event.getY().toInt()
-            } else if (event.action == MotionEvent.ACTION_UP) {
-                x2 = event.getX().toInt()
-                y2 = event.getY().toInt()
-                dx = x2-x1
-                dy = y2-y1
-
-                //TODO: Make this function take distance/angle into account
-
-                if(Math.abs(dx) > Math.abs(dy))
-                {
-                    if (dx>0){
-                        changePage(-1)
-                    }
-                    else if (dx<0)
-                    {
-                        changePage(1)
-                    }
-                }
-                else
-                {
-                    if (dy>0)   //up
-                        changeTokens(-1,2)
-                    else if (dy<0)  //down
-                        changeTokens(1,2)
-                }
-            }
-            true
-        })
-
-        token3?.setOnTouchListener(OnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                x1 = event.getX().toInt()
-                y1 = event.getY().toInt()
-            } else if (event.action == MotionEvent.ACTION_UP) {
-                x2 = event.getX().toInt()
-                y2 = event.getY().toInt()
-                dx = x2-x1
-                dy = y2-y1
-
-                //TODO: Make this function take distance/angle into account
-
-                if(Math.abs(dx) > Math.abs(dy))
-                {
-                    if (dx>0){
-                        changePage(-1)
-                    }
-                    else if (dx<0)
-                    {
-                        changePage(1)
-                    }
-                }
-                else
-                {
-                    if (dy>0)   //up
-                        changeTokens(-1,3)
-                    else if (dy<0)  //down
-                        changeTokens(1,3)
-                }
-            }
-            true
-        })
-
         downButton?.setOnClickListener{changeHealth(-1)}
         leftButton?.setOnClickListener{changePage(-1)}
         rightButton?.setOnClickListener{changePage(1)}
@@ -505,43 +335,26 @@ class HealthFragment : Fragment() {
         token3DownBtn?.setOnClickListener{changeTokens(-1,3)}
 
         when (this.tag) {
-            "player1Fragment" -> {
-                playNo = 1
-            }
-            "player2Fragment" -> {
-                playNo = 2
-            }
-            "player3Fragment" -> {
-                playNo = 3
-            }
-            "player4Fragment" -> {
-                playNo = 4
-            }
+            "player1Fragment" -> playNo = 1
+            "player2Fragment" -> playNo = 2
+            "player3Fragment" -> playNo = 3
+            "player4Fragment" -> playNo = 4
         }
-
-        val handler = Handler()
-        handler.postDelayed(Runnable {
-            // yourMethod();
-            var text = "Test"
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(context, text, duration)
-            toast.show()
-
-        }, 1000) //5 seconds
-
     }
 
-    fun getDirectionOfSwipe(event:MotionEvent) : String {
+    private fun getDirectionOfSwipe(event:MotionEvent) : String {
+        var x1 = 0
+        var y1 = 0
         if (event.action == MotionEvent.ACTION_DOWN) {
-            x1 = event.getX().toInt()
-            y1 = event.getY().toInt()
+            x1 = event.x.toInt()
+            y1 = event.y.toInt()
         } else if (event.action == MotionEvent.ACTION_UP) {
-            x2 = event.getX().toInt()
-            y2 = event.getY().toInt()
-            dx = x2-x1
-            dy = y2-y1
+            val x2 = event.x.toInt()
+            val y2 = event.y.toInt()
+            val dx = x2-x1
+            val dy = y2-y1
 
-            if(Math.abs(dx) > Math.abs(dy))
+            if(abs(dx) > abs(dy))
             {
                 if (dx>0){
                     changePage(-1)
@@ -553,14 +366,12 @@ class HealthFragment : Fragment() {
             }
             else
             {
-                if (dy>150)   //up
-                    return "bigUp"
-                else if (dy<-150)  //down
-                    return "bigDown"
-                else if (dy>0)   //up
-                    return "up"
-                else if (dy<0)  //down
-                    return "down"
+                when {
+                    dy>150  -> return "bigUp"
+                    dy<-150 -> return "bigDown"
+                    dy>0    -> return "up"
+                    dy<0    -> return "down"
+                }
             }
         }
         return ""
